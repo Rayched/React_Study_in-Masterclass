04f688c7eaf029b34040f92697c5daba44643600
Rayched <ny2401@naver.com>
2025-11-04 14:57:28 +0900

    20251104, react-router-v6 수강 준비

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export default function App(){
+    return (
+        <Wrapper>
+            <h4>Hello World</h4>
+        </Wrapper>
+    );
+};
